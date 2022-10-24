bae1fe9529aff9b8309960be238a969bfc3b855e
lpas <lukasz.pasek7@wp.pl>
2022-10-24 23:39:30 +0200

    Added synchronized tasks Added Collection binding

--- InPost/ViewModels/MainViewModel.cs
+++ InPost/ViewModels/MainViewModel.cs
@@ -6,9 +6,12 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Markup;
 
 namespace InPost.ViewModels
 {
@@ -17,8 +20,9 @@ namespace InPost.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
         public ICommand NadajPaczkeCommand { get; set; }
         public ICommand OdbierzPaczkeCommand { get; set; }
-        public Paczkomat PaczkomatMain = new Paczkomat(1);
+        public Paczkomat PaczkomatMain { get; set; }
         Task ZadanieZKolejki;
+        SynchronizationContext uiContext;
         public ObservableCollection<OperacjaViewModel> MainHistory { get; set; }
         public string Text { get; set; }
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -28,28 +32,46 @@ namespace InPost.ViewModels
         public MainViewModel()
         {
             Text = "Paczkomat!!!";
+            MainHistory = new ObservableCollection<OperacjaViewModel>();
+            PaczkomatMain = new Paczkomat(1);
+            uiContext =  SynchronizationContext.Current;
+            ZadanieZKolejki = new Task(() => PaczkomatMain.ObsluzInteresanta());
             NadajPaczkeCommand = new RelayCommand(o => NadajPaczkeClick("Nadałeś paczkę!"));
             OdbierzPaczkeCommand = new RelayCommand(o => OdbierzPaczkeClick("Odebrałeś paczkę!"));
-            ZadanieZKolejki = new Task(() => PaczkomatMain.ObsluzInteresanta());
-            ZadanieZKolejki.Start();
+            //ZadanieZKolejki = new Task(() => PaczkomatMain.ObsluzInteresanta());
+            //ZadanieZKolejki.Start();
         }
 
-        private void NadajPaczkeClick(object sender)
+        private async void NadajPaczkeClick(object sender)
         {
             Paczka x = new Paczka("Łukasz", "Pasek", "Mateusz", "Pasek", 222);
             Nadanie y = new Nadanie(x, 1);
             PaczkomatMain.UstawDoKolejki(y);
             //ZadanieZKolejki.Start();
-            //if (PaczkomatMain.Q.IsEmpty) ZadanieZKolejki.Wait();
+            while (!PaczkomatMain.Q.IsEmpty)
+            {
+                 //Task.Run(() => PaczkomatMain.ObsluzInteresanta());
+                 uiContext.Send(x => PaczkomatMain.ObsluzInteresanta(), null);
+            }
             MessageBox.Show(sender.ToString());
+            //PaczkomatMain.History.Add(new OperacjaViewModel(1, y));
+            //MainHistory.Add(new OperacjaViewModel(1, y));
+            //MainHistory = PaczkomatMain.History;
         }
         private void OdbierzPaczkeClick(object sender)
         {
-            Odebranie y = new Odebranie(222,1);
+            Odebranie y = new Odebranie(222,2);
             PaczkomatMain.UstawDoKolejki(y);
+
+            while (!PaczkomatMain.Q.IsEmpty)
+            {
+                uiContext.Send(x => PaczkomatMain.ObsluzInteresanta(), null);
+            }
             //ZadanieZKolejki.Start();
             //if (PaczkomatMain.Q.IsEmpty) ZadanieZKolejki.Wait();
             MessageBox.Show(sender.ToString());
+            //MainHistory = PaczkomatMain.History;
+
         }
     }
 }
--- InPost/ViewModels/OperacjaViewModel.cs
+++ InPost/ViewModels/OperacjaViewModel.cs
@@ -7,11 +7,25 @@ using System.Threading.Tasks;
 
 namespace InPost.ViewModels
 {
-    
+
     public class OperacjaViewModel
     {
         public int NrPaczkomatu;
         public string Numer => NrPaczkomatu.ToString();
+        public string TypOperacji
+        {
+            get
+            {
+                if (Operacja is Nadanie)
+                {
+                    return "Nadanie";
+                }
+                else
+                {
+                    return "Odebranie";
+                }
+            }
+        }
         public IOperacja Operacja;
         public OperacjaViewModel(int nrPaczkomatu, IOperacja operacja)
         {
